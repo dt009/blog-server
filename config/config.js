0032aa1fa7ffcaa8809b5a50bd85f27439e23f76
dt009 <18363625031@163.com>
2018-08-24 13:48:01 +0800

    添加 config

--- config/config.js
+++ config/config.js
@@ -2,12 +2,19 @@
  * @Author: duantao-ds
  * @Date: 2018-08-24 11:32:25
  * @Last Modified by: duantao-ds
- * @Last Modified time: 2018-08-24 11:33:20
+ * @Last Modified time: 2018-08-24 12:14:54
  */
 
 
 const config = {
-    serverPort: '8080'
+    serverPort: '8080',
+    database: {
+        DATABASE: 'blog_sql',
+        USERNAME: 'root',
+        PASSWORD: 'root',
+        HOST: 'localhost',
+        PORT: '3306'
+    }
 }
 
 module.exports = config;
