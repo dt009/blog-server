0f95c01f2a402fefc2819e6ae3c86b72c961b8ec
dt009 <18363625031@163.com>
2018-09-12 15:36:34 +0800

    修改配置

--- config/config.js
+++ config/config.js
@@ -2,18 +2,38 @@
  * @Author: duantao-ds
  * @Date: 2018-08-24 11:32:25
  * @Last Modified by: duantao-ds
- * @Last Modified time: 2018-08-24 12:14:54
+ * @Last Modified time: 2018-09-12 15:35:40
  */
 
+const os = require('os');
 
-const config = {
-    serverPort: '8080',
-    database: {
-        DATABASE: 'blog_sql',
-        USERNAME: 'root',
-        PASSWORD: 'root',
-        HOST: 'localhost',
-        PORT: '3306'
+let hostname = os.hostname;
+
+let config;
+
+if (hostname === 'instance-lfnr0pzg') {
+    config = {
+        serverPort: '8080',
+        database: {
+            DATABASE: 'blog_sql',
+            USERNAME: 'root',
+            PASSWORD: 'Iamdt@009',
+            HOST: 'localhost',
+            PORT: '3306'
+        }
+    }
+
+}
+else {
+    config = {
+        serverPort: '8080',
+        database: {
+            DATABASE: 'blog_sql',
+            USERNAME: 'root',
+            PASSWORD: 'root',
+            HOST: 'localhost',
+            PORT: '3306'
+        }
     }
 }
 
