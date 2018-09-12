0161f98d9028640a7f6a646aef278571a772679f
dt009 <18363625031@163.com>
2018-09-12 14:30:56 +0800

    添加前端部分

--- router/home/home.js
+++ router/home/home.js
@@ -2,7 +2,7 @@
  * @Author: duantao-ds
  * @Date: 2018-08-24 11:38:19
  * @Last Modified by: duantao-ds
- * @Last Modified time: 2018-08-24 11:46:36
+ * @Last Modified time: 2018-09-12 11:24:08
  */
 
 const Router = require('koa-router');
@@ -11,10 +11,7 @@ const router = new Router;
 
 // 主页请求
 router.get('/', async ctx => {
-    await ctx.render('index', {
-        title: '测试',
-        test: '这是测试文本'
-    })
+    await ctx.render('index', {})
 });
 
 module.exports = router;
